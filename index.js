495a3045a64b7ff55ce0aa3c65533ea6204209e0
hepanazure <hepan.azure@students.msswarriors.org>
2026-05-06 16:33:43 +0000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function calculateAward(points) {
+    if (points > 99) {
+        return "First Place";
+    }
+
+    if (points > 49) {
+        return "Second Place";
+    }
+
+    return "Participation Award";
+}
+
+const resultElement = document.getElementById("result");
+if (resultElement) {
+    resultElement.textContent = calculateAward(49);
+}
 
